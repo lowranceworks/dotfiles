a14cadfbe3cba60ee0ed85c82fc0805359469b44
lowranceworks <lowranceworks@gmail.com>
2024-04-14 08:14:15 -0500

    init commit

---
+++ fish/conf.d/alias.fish
@@ -0,0 +1,11 @@
+alias python python3
+alias vim nvim
+alias docker podman
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
+
+alias ll="lsd --group-dirs first -Al"
---
+++ wezterm/utils/wallpaper.lua
@@ -0,0 +1,60 @@
+local wezterm = require("wezterm")
+local h = require("utils/helpers")
+local M = {}
+
+M.get_wallpaper = function()
+	local wallpapers = {}
+	local wallpapers_glob = os.getenv("HOME")
+		.. "/Library/Mobile Documents/com~apple~CloudDocs/PARA/Resources 🧰/Wallpapers - macOS 💻/active/**"
+	for _, v in ipairs(wezterm.glob(wallpapers_glob)) do
+		if not string.match(v, "%.DS_Store$") then
+			table.insert(wallpapers, v)
+		end
+	end
+	local wallpaper = h.get_random_entry(wallpapers)
+	if wallpaper == nil then
+		-- Handle the case when no wallpaper is found
+		-- For example, return a default wallpaper or an empty table
+		return {}
+	end
+	return {
+		source = { File = { path = wallpaper } },
+		height = "Cover",
+		width = "Cover",
+		horizontal_align = "Left",
+		repeat_x = "Repeat",
+		repeat_y = "Repeat",
+		opacity = 1,
+		-- speed = 200,
+	}
+end
+
+M.set_nvim_wallpaper = function(name)
+	return {
+		source = { File = { path = os.getenv("HOME") .. "/.config/wezterm/wallpapers/nvim/" .. name } },
+		height = "Cover",
+		width = "Cover",
+		horizontal_align = "Center",
+		repeat_x = "Repeat",
+		repeat_y = "Repeat",
+		opacity = 1,
+		-- speed = 200,
+	}
+end
+
+M.set_tmux_session_wallpaper = function(value)
+	local path = os.getenv("HOME") .. "/.config/wezterm/wallpapers/sessions/" .. value .. ".jpeg"
+	print(path)
+	return {
+		source = { File = { path = path } },
+		height = "Cover",
+		width = "Cover",
+		horizontal_align = "Center",
+		repeat_x = "Repeat",
+		repeat_y = "Repeat",
+		opacity = 1,
+		-- speed = 200,
+	}
+end
+
+return M
