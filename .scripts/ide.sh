6c5e12667a1e08d667298de50f2066b11311f809
lowranceworks <lowranceworks@gmail.com>
2024-05-10 08:55:31 -0500

    fix: update ide pane layout

--- .scripts/ide.sh
+++ .scripts/ide.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-tmux split-window -h -l 66%
-tmux select-pane -t 0
+tmux split-window -h -l 75%
 tmux send-keys -t 0 'clear' Enter
+tmux send-keys -t 0 'lazygit' Enter
 tmux send-keys -t 1 'nvim .' Enter
 tmux select-pane -t 1
