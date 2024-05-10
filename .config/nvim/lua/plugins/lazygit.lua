ae87f6c63e76da6ece33cb7314540dbd9e9af667
lowranceworks <lowranceworks@gmail.com>
2024-05-10 13:49:20 -0500

    feat: add lazygit plugin

---
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,19 @@
+-- nvim v0.8.0
+require("lazy").setup({
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+})
