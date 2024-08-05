ec9ca9d7ae43f26fa1865403ee792c64431de79f
lowranceworks <lowranceworks@gmail.com>
2024-08-05 09:50:51 -0500

    fix: nix

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -14,25 +14,22 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
     configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ 
-          pkgs.vim
-          pkgs.direnv
-          pkgs.age
-          pkgs.sshs
-          pkgs.atac
-          pkgs.termshark
-          pkgs.portal
-          pkgs.glow
-        ];
+      environment.systemPackages = [
+        pkgs.vim
+        pkgs.direnv
+        pkgs.age
+        pkgs.sshs
+        pkgs.atac
+        pkgs.termshark
+        pkgs.portal
+        pkgs.glow
+      ];
       services.nix-daemon.enable = true;
       nix.settings.experimental-features = "nix-command flakes";
-      programs.zsh.enable = true;  # default shell on catalina
+      programs.zsh.enable = true;
+      programs.fish.enable = true;
       system.configurationRevision = self.rev or self.dirtyRev or null;
       system.stateVersion = 4;
-      nixpkgs.hostPlatform = "aarch64-darwin";
       security.pam.enableSudoTouchIdAuth = true;
 
       users.users.josh.home = "/Users/josh";
@@ -50,22 +47,21 @@
         screensaver.askForPasswordDelay = 10;
       };
 
-      # Homebrew needs to be installed on its own!
       homebrew.enable = true;
       homebrew.casks = [
-	      "wireshark"
+              "wireshark"
               "google-chrome"
       ];
       homebrew.brews = [
-	      "imagemagick"
+              "imagemagick"
       ];
     };
   in
   {
     darwinConfigurations."Joshs-MacBook-Pro" = nix-darwin.lib.darwinSystem {
       system = "aarch64-darwin";
-      modules = [ 
-	configuration
+      modules = [
+        configuration
         home-manager.darwinModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
@@ -74,7 +70,6 @@
       ];
     };
 
-    # Expose the package set, including overlays, for convenience.
     darwinPackages = self.darwinConfigurations."Joshs-MacBook-Pro".pkgs;
   };
 }
--- .config/nix-darwin/home.nix
+++ .config/nix-darwin/home.nix
@@ -1,28 +1,30 @@
 # home.nix
-# home-manager switch 
+# home-manager switch
 
 { config, pkgs, ... }:
 
+let
+  dotfilesPath = builtins.path {
+    path = "/Users/josh/.config/";
+    name = "dotfiles";
+  };
+in
 {
   home.username = "josh";
   home.homeDirectory = "/Users/josh";
-  home.stateVersion = "23.05"; # Please read the comment before changing.
+  home.stateVersion = "23.05";
 
-# Makes sense for user specific applications that shouldn't be available system-wide
   home.packages = [
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    ".config/fish".source = ~/dotfiles/fish;
-    ".config/wezterm".source = ~/dotfiles/wezterm;
-    ".config/skhd".source = ~/dotfiles/skhd;
-    ".config/starship".source = ~/dotfiles/starship;
-    ".config/nvim".source = ~/dotfiles/nvim;
-    ".config/nix".source = ~/dotfiles/nix;
-    ".config/nix-darwin".source = ~/dotfiles/nix-darwin;
-    ".config/tmux".source = ~/dotfiles/tmux;
+    ".config/wezterm".source = "${dotfilesPath}/wezterm";
+    ".config/skhd".source = "${dotfilesPath}/skhd";
+    ".config/starship".source = "${dotfilesPath}/starship";
+    ".config/nvim".source = "${dotfilesPath}/nvim";
+    ".config/nix".source = "${dotfilesPath}/nix";
+    ".config/nix-darwin".source = "${dotfilesPath}/nix-darwin";
+    ".config/tmux".source = "${dotfilesPath}/tmux";
   };
 
   home.sessionVariables = {
@@ -36,7 +38,6 @@
   programs.zsh = {
     enable = true;
     initExtra = ''
-      # Add any additional configurations here
       export PATH=/run/current-system/sw/bin:$HOME/.nix-profile/bin:$PATH
       if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
         . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
