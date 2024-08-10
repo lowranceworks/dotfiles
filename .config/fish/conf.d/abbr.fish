6ee2c5d0c1daad15115b5d918e9129aef386f95a
lowranceworks <lowranceworks@gmail.com>
2024-08-10 11:27:09 -0500

    chore: remove fish.abbrvs

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -2,9 +2,6 @@
 # NOTE: manage fish abbreviations
 # https://fishshell.com/docs/current/cmds/abbr.html
 
-abbr ast "aw set -t (aw list | fzf-tmux -p --reverse --preview 'aw set -t {}')"
-abbr av "NVIM_APPNAME=astronvim nvim"
-
 abbr bi "brew install"
 abbr bic "brew install --cask"
 abbr bin "brew info"
@@ -13,8 +10,6 @@ abbr bl "brew leaves"
 abbr blr "brew leaves --installed-on-request"
 abbr blp "brew leaves --installed-as-dependency"
 abbr bs "brew search"
-
-abbr bni "bun i"
 
 abbr c clear
 abbr cl clear
@@ -30,8 +25,6 @@ abbr dcu "docker compose up -d"
 abbr dps "docker ps --format 'table {{.Names}}\t{{.Status}}'"
 
 abbr e exit
-abbr ee "espanso edit"
-abbr er "espanso restart"
 
 abbr fi "fisher install"
 abbr fr "fisher refresh"
@@ -100,9 +93,6 @@ abbr kl "kubectl logs -f"
 abbr ke "kubectl exec -it"
 abbr kcns "kubectl config set-context --current --namespace"
 
-abbr ka killall
-abbr kn "killall node"
-
 abbr l "lsd  --group-dirs first -A"
 abbr ld lazydocker
 abbr lg lazygit
@@ -119,59 +109,22 @@ abbr ne "nvim .env"
 abbr nf neofetch
 abbr ni "npm install"
 abbr nt "npm run test"
-abbr nxdg "nx dep-graph"
-abbr ns "nu seed"
 
 abbr o "open ."
-abbr oc "overmind connect (overmind ps | fzf | awk '{print $1}')"
-abbr ok "overmind kill"
-abbr or "overmind restart"
-abbr os "overmind start -D"
-abbr osl "overmind start -l"
 
 abbr p podman
-abbr pa "pnpm add"
-abbr pb "pnpm build"
-abbr pd "pnpm dev"
-abbr pe "pnpm e2e"
-abbr pg "pnpm generate"
-abbr ph "pnpm help"
-abbr pi "pnpm install"
-abbr pim "pnpm import"
-abbr pir "pnpm rebuild"
-abbr pit "pnpm install-test"
-abbr pl "pnpm lint"
-abbr pls "pnpm ls"
-abbr pr "pnpm run"
-abbr ps "pnpm start"
-abbr psa "pnpm store add"
-abbr psp "pnpm store prune"
-abbr pss "pnpm store status"
-abbr pt "pnpm test"
-abbr pu "pnpm update"
-abbr pul "pnpm unlink"
-abbr pw "pnpm web"
-abbr pwdc "pwd | pbcopy"
 
 abbr rmr "rm -rf"
 
-abbr s "sesh connect (sesh list | zf --height 24)"
-abbr sa "SwitchAudioSource -t output -s (SwitchAudioSource -t output -a | fzf-tmux -p --reverse)"
-abbr sai "SwitchAudioSource -t input -s (SwitchAudioSource -t input -a | fzf-tmux -p --reverse)"
-abbr sao "SwitchAudioSource -t output -s (SwitchAudioSource -t output -a | fzf-tmux -p --reverse)"
-abbr sb "sam build"
 abbr sf "source ~/.config/fish/config.fish"
 abbr st "tmux source ~/.config/tmux/tmux.conf"
 
-abbr th things-cli
 abbr t tmux
 abbr ta "tmux a"
 abbr tat "tmux attach -t"
 abbr td "t dotfiles"
 abbr tk "tmux kill-server"
 abbr tks "tmux kill-server"
-abbr tr "tldr --list | fzf --header 'tldr (tealdeer)' --reverse --preview 'tldr {1} --color=always' --preview-window=right,80% | xargs tldr"
-abbr tp "t --repo (pbpaste)"
 abbr tn "tmux new -s (basename (pwd))"
 abbr tt "touch .t && chmod +x .t && echo -e '#!/usr/bin/env bash\n' > .t && nvim .t"
 
@@ -193,24 +146,6 @@ abbr tg terragrunt
 
 abbr vim nvim
 abbr v "nvim ."
-
-abbr x "chmod +x (ls | gum filter --limit 1 --header 'chmod +x')"
-
-abbr y yarn
-abbr ya "yarn add"
-abbr yad "yarn add -D"
-abbr yb "yarn build"
-abbr yd "yarn dev"
-abbr ye "yarn e2e"
-abbr yg "yarn generate"
-abbr yi "yarn install --frozen-lockfile"
-abbr yl "yarn lint"
-abbr yp "yarn plop"
-abbr ypm "yarn plop model"
-abbr ys "yarn server"
-abbr yt "yarn test"
-abbr yu "yarn ui"
-abbr yw "yarn web"
 
 abbr za "zoxide add"
 abbr ze "zoxide edit"
