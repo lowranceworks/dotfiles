96c677e2305b9f0ae4feb171bdd15c387b4daae7
lowranceworks <lowranceworks@gmail.com>
2024-08-30 11:03:40 -0500

    fix: nvim: rm swap with leader a (conflicts with avante)

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -75,15 +75,6 @@ return {
           ["[]"] = "@class.outer",
         },
       },
-      swap = {
-        enable = true,
-        swap_next = {
-          ["<leader>a"] = "@parameter.inner",
-        },
-        swap_previous = {
-          ["<leader>A"] = "@parameter.inner",
-        },
-      },
     },
   }),
 }
