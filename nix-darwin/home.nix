1d4364e916ae9dae6acced1b313c82540100a9f1
lowranceworks <lowranceworks@gmail.com>
2024-10-18 22:45:17 -0500

    fix: joshua -> josh

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -1,27 +1,27 @@
 { config, pkgs, dotfiles, ... }:
 {
   # TODO: find a way to do this without hardcoding the user
-  home.username = "joshua";
-  home.homeDirectory = "/Users/joshua";
+  home.username = "josh";
+  home.homeDirectory = "/Users/josh";
   home.stateVersion = "23.05";
 
   home.packages = [
     # Add any packages you want to install
   ];
 
-  home.file = {
-    ".skhdrc".source = "${dotfiles}/skhd/.skhdrc";
-    ".yabairc".source = "${dotfiles}/yabai/.yabairc";
-    ".config/starship.toml".source = "${dotfiles}/starship/starship.toml";
-    ".config/fish".source = "${dotfiles}/fish";
-    ".config/wezterm".source = "${dotfiles}/wezterm";
-    ".config/lazygit".source = "${dotfiles}/lazygit";
-    ".config/neofetch".source = "${dotfiles}/neofetch";
-    ".config/nvim".source = "${dotfiles}/nvim";
-    ".config/nix".source = "${dotfiles}/nix";
-    ".config/nix-darwin".source = "${dotfiles}/nix-darwin";
-    ".config/tmux".source = "${dotfiles}/tmux";
-  };
+  # home.file = {
+  #   ".skhdrc".source = "${dotfiles}/skhd/.skhdrc";
+  #   ".yabairc".source = "${dotfiles}/yabai/.yabairc";
+  #   ".config/starship.toml".source = "${dotfiles}/starship/starship.toml";
+  #   ".config/fish".source = "${dotfiles}/fish";
+  #   ".config/wezterm".source = "${dotfiles}/wezterm";
+  #   ".config/lazygit".source = "${dotfiles}/lazygit";
+  #   ".config/neofetch".source = "${dotfiles}/neofetch";
+  #   ".config/nvim".source = "${dotfiles}/nvim";
+  #   ".config/nix".source = "${dotfiles}/nix";
+  #   ".config/nix-darwin".source = "${dotfiles}/nix-darwin";
+  #   ".config/tmux".source = "${dotfiles}/tmux";
+  # };
 
   home.sessionVariables = {
     PATH = "$PATH:/run/current-system/sw/bin:$HOME/.nix-profile/bin";
