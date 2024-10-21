9bcc07742ded201b1b1165038d796f0aa748cf16
lowranceworks <lowranceworks@gmail.com>
2024-10-21 12:13:29 -0500

    fix: format/lint with 4 spaces instead of 2

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -17,9 +17,9 @@ vim.wo.signcolumn = "yes"
 vim.o.completeopt = "menuone,noselect"
 
 -- Indenting
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
