0362dc2a2f01798ca514a1681f5eae988c3b9610
lowranceworks <lowranceworks@gmail.com>
2024-10-21 22:10:48 -0500

    chore: cleanup

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -19,190 +19,195 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, dotfiles }:
   let
     configuration = { pkgs, ... }: {
-      ids.gids.nixbld = 350; # NOTE: this is required on my personal MacBook
+        ids.gids.nixbld = 350; # NOTE: this is required on my personal MacBook
 
-      # Add Fish to /etc/shells
-      environment.shells = [
-        "/opt/homebrew/bin/fish"
-      ];
-
-      environment.loginShell = "/opt/homebrew/bin/fish";  
-      environment.systemPackages = with pkgs; [
-        vim
-        direnv
-      ];
-      services.nix-daemon.enable = true;
-      nix.settings.experimental-features = "nix-command flakes";
-
-      programs.zsh = {
-        enable = false;
-        shellInit = ''
-          # Optional initialization
-        '';
-      };
-
-      # TODO: figure out how to set fish as the default shell
-      programs.fish =  {
-        enable = true;
-        useBabelfish = true; # Use babelfish for better environment variable handling - Translate bash scripts to fish.
-        vendor = {
-          completions.enable = true;
-          config.enable = true;
-          functions.enable = true;
-        };
-        shellInit = ''
-          # Optional initialization
-          '';
-      };
-
-
-      users.users.josh = {
-        home = "/Users/josh";
-        shell = "/opt/homebrew/bin/fish"; # Set fish as the default shell (not working)
-      };
-
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-      system.stateVersion = 4;
-      security.pam.enableSudoTouchIdAuth = true;
-
-      home-manager.backupFileExtension = "backup";
-      nix.configureBuildUsers = true;
-      nix.useDaemon = true;
-
-      system.defaults = {
-        dock.orientation = "right"; # Set the dock to the right side of the screen.
-        dock.autohide = true; # Automatically hide the dock.
-        dock.mru-spaces = false; # ?
-        dock.persistent-apps = [ # Applications that should always be in the dock.
-          "/Applications/WezTerm.app/"
-          "/Applications/Arc.app/"
-          "/Applications/Inkdrop.app"
-          "/System/Applications/Calendar.app/"
-          "/Applications/Bitwarden.app/"
-          "/Applications/draw.io.app/"
-          "/System/Applications/Launchpad.app/"
-          # "/System/Applications/Utilities/Terminal.app"
+        # Add Fish to /etc/shells
+        environment.shells = [
+          "/opt/homebrew/bin/fish"
         ];
 
-        loginwindow.LoginwindowText = "Let's Go!";
-        screensaver.askForPasswordDelay = 10; # How long to wait before asking for a password.
+        environment.loginShell = "/opt/homebrew/bin/fish";  
+        environment.systemPackages = with pkgs; [
+          vim
+          direnv
+        ];
 
-        finder.ShowPathbar = true; # Show path breadcrumbs in finder windows.
-        finder.AppleShowAllFiles = true; # Always show hidden files.
-        finder.FXPreferredViewStyle = "clmv"; # Change the default finder view to Column view.
-        finder.AppleShowAllExtensions = true; # Whether to always show file extensions. The default is false.
+        services.nix-daemon.enable = true;
+        nix.settings.experimental-features = "nix-command flakes";
 
-        NSGlobalDomain.AppleInterfaceStyle = "Dark"; # Enable dark mode.
-        NSGlobalDomain.InitialKeyRepeat = 17; # How long you must hold down the key before it starts repeating.
-        NSGlobalDomain.KeyRepeat = 2; # How fast it repeats once it starts.
-        NSGlobalDomain._HIHideMenuBar = true; # Hide the menu bar.
+        # Set up TPM
+        system.activationScripts.postActivation.text = ''
+          mkdir -p ~/.tmux/plugins
+          ln -sfn ${config._module.args.tpm} ~/.tmux/plugins/tpm
+        '';
+
+        programs.zsh = {
+          enable = false;
+          shellInit = ''
+            # Optional initialization
+          '';
+        };
+
+        # TODO: figure out how to set fish as the default shell
+        programs.fish =  {
+          enable = true;
+          useBabelfish = true; # Use babelfish for better environment variable handling - Translate bash scripts to fish.
+          vendor = {
+            completions.enable = true;
+            config.enable = true;
+            functions.enable = true;
+          };
+          shellInit = ''
+            # Optional initialization
+            '';
+        };
+
+        users.users.josh = {
+          home = "/Users/josh";
+          shell = "/opt/homebrew/bin/fish"; # Set fish as the default shell (not working)
+        };
+
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+        system.stateVersion = 4;
+        security.pam.enableSudoTouchIdAuth = true;
+
+        home-manager.backupFileExtension = "backup";
+        nix.configureBuildUsers = true;
+        nix.useDaemon = true;
+
+        system.defaults = {
+          dock.orientation = "right"; # Set the dock to the right side of the screen.
+          dock.autohide = true; # Automatically hide the dock.
+          dock.mru-spaces = false; # ?
+          dock.persistent-apps = [ # Applications that should always be in the dock.
+            "/Applications/WezTerm.app/"
+            "/Applications/Arc.app/"
+            "/Applications/Inkdrop.app"
+            "/System/Applications/Calendar.app/"
+            "/Applications/Bitwarden.app/"
+            "/Applications/draw.io.app/"
+            "/System/Applications/Launchpad.app/"
+            # "/System/Applications/Utilities/Terminal.app"
+          ];
+
+          loginwindow.LoginwindowText = "Let's Go!";
+          screensaver.askForPasswordDelay = 10; # How long to wait before asking for a password.
+
+          finder.ShowPathbar = true; # Show path breadcrumbs in finder windows.
+          finder.AppleShowAllFiles = true; # Always show hidden files.
+          finder.FXPreferredViewStyle = "clmv"; # Change the default finder view to Column view.
+          finder.AppleShowAllExtensions = true; # Whether to always show file extensions. The default is false.
+
+          NSGlobalDomain.AppleInterfaceStyle = "Dark"; # Enable dark mode.
+          NSGlobalDomain.InitialKeyRepeat = 17; # How long you must hold down the key before it starts repeating.
+          NSGlobalDomain.KeyRepeat = 2; # How fast it repeats once it starts.
+          NSGlobalDomain._HIHideMenuBar = true; # Hide the menu bar.
+        };
+
+        homebrew.enable = true;
+        homebrew.casks = [
+          "arc"
+          "alfred"
+          "bitwarden"
+          # "brave-browser"
+          # "choosy"
+          # "chromium"
+          "cleanshot"
+          "contexts"
+          "drawio"
+          "fork"
+          "google-chrome"
+          "google-cloud-sdk"
+          "inkdrop"
+          "keycastr"
+          # "logitech-options"
+          "meetingbar"
+          # "microsoft-auto-update"
+          # "microsoft-excel"
+          # "microsoft-outlook"
+          # "microsoft-teams"
+          "mission-control-plus"
+          "paintbrush"
+          # "podman-desktop"
+          # "rectangle-pro"
+          # "sensiblesidebuttons"
+          # "sf-symbols" # NOTE: commented out because it prompts for password
+          "slack"
+          "spacelauncher"
+          "stats"
+          "utm"
+          "wezterm"
+          "betterdisplay"
+          # "homebrew/cask-fonts/font-fontawesome" # WARN: this fails to install because it is deprecated 
+          # "homebrew/cask-fonts/font-hack-nerd-font" # WARN: this fails to install because it is deprecated 
+        ];
+        homebrew.brews = [
+          "act"
+          "ansible"
+          "argocd"
+          "awscli"
+          "azure-cli"
+          "bash"
+          "checkov"
+          "cryptography"
+          "direnv"
+          "docker"
+          "docker-completion"
+          "docker-compose"
+          "docutils"
+          "dtc"
+          "fd"
+          "fish"
+          # "fisher"
+          "fontconfig"
+          "fzf"
+          "gh"
+          "gnu-sed"
+          "go"
+          "go-task"
+          "helm"
+          "jq"
+          "kind"
+          "krew"
+          "kubectx"
+          "kubernetes-cli"
+          "kustomize"
+          "lazydocker"
+          "lazygit"
+          "lua"
+          "make"
+          "neofetch"
+          "neovim"
+          "node"
+          "podman"
+          "pre-commit"
+          "pyenv"
+          "qemu"
+          "ripgrep"
+          "sops"
+          "starship"
+          "stern"
+          "telnet"
+          "terraform-docs"
+          "terragrunt"
+          "terrascan"
+          "tfenv"
+          "tflint"
+          "tfsec"
+          "tfupdate"
+          "tldr"
+          "tmux"
+          "yq"
+          "yubikey-agent"
+          "zoxide"
+          "zsh-autosuggestions"
+          "zsh-fast-syntax-highlighting"
+          "zsh-vi-mode"
+          "zstd"
+          "federico-terzi/espanso/espanso" # not found, tap it and try again
+          "minamijoyo/hcledit/hcledit" # not found, tap it and try again
+        ];
       };
-
-      homebrew.enable = true;
-      homebrew.casks = [
-        "arc"
-        "alfred"
-        "bitwarden"
-        # "brave-browser"
-        # "choosy"
-        # "chromium"
-        "cleanshot"
-        "contexts"
-        "drawio"
-        "fork"
-        "google-chrome"
-        "google-cloud-sdk"
-        "inkdrop"
-        "keycastr"
-        # "logitech-options"
-        "meetingbar"
-        # "microsoft-auto-update"
-        # "microsoft-excel"
-        # "microsoft-outlook"
-        # "microsoft-teams"
-        "mission-control-plus"
-        "paintbrush"
-        # "podman-desktop"
-        # "rectangle-pro"
-        # "sensiblesidebuttons"
-        # "sf-symbols" # NOTE: commented out because it prompts for password
-        "slack"
-        "spacelauncher"
-        "stats"
-        "utm"
-        "wezterm"
-        "betterdisplay"
-        # "homebrew/cask-fonts/font-fontawesome" # WARN: this fails to install because it is deprecated 
-        # "homebrew/cask-fonts/font-hack-nerd-font" # WARN: this fails to install because it is deprecated 
-
-      ];
-      homebrew.brews = [
-        "act"
-        "ansible"
-        "argocd"
-        "awscli"
-        "azure-cli"
-        "bash"
-        "checkov"
-        "cryptography"
-        "direnv"
-        "docker"
-        "docker-completion"
-        "docker-compose"
-        "docutils"
-        "dtc"
-        "fd"
-        "fish"
-        # "fisher"
-        "fontconfig"
-        "fzf"
-        "gh"
-        "gnu-sed"
-        "go"
-        "go-task"
-        "helm"
-        "jq"
-        "kind"
-        "krew"
-        "kubectx"
-        "kubernetes-cli"
-        "kustomize"
-        "lazydocker"
-        "lazygit"
-        "lua"
-        "make"
-        "neofetch"
-        "neovim"
-        "node"
-        "podman"
-        "pre-commit"
-        "pyenv"
-        "qemu"
-        "ripgrep"
-        "sops"
-        "starship"
-        "stern"
-        "telnet"
-        "terraform-docs"
-        "terragrunt"
-        "terrascan"
-        "tfenv"
-        "tflint"
-        "tfsec"
-        "tfupdate"
-        "tldr"
-        "tmux"
-        "yq"
-        "yubikey-agent"
-        "zoxide"
-        "zsh-autosuggestions"
-        "zsh-fast-syntax-highlighting"
-        "zsh-vi-mode"
-        "zstd"
-        "federico-terzi/espanso/espanso" # not found, tap it and try again
-        "minamijoyo/hcledit/hcledit" # not found, tap it and try again
-      ];
-    };
   in
   {
     darwinConfigurations."LowranceWorks" = nix-darwin.lib.darwinSystem {
