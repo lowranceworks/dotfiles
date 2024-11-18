99e401301dec1921511d39b291b6fa92ee7e4396
lowranceworks <lowranceworks@gmail.com>
2024-11-18 12:50:11 -0600

    fix: sketchybar

---
+++ sketchybar/colors.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Catppuccin Mocha Colors
+export WHITE=0xffcdd6f4         # Text
+export BAR_COLOR=0xff1e1e2e     # Base
+export ITEM_BG_COLOR=0xff313244 # Surface0
+export ACCENT_COLOR=0xff89b4fa  # Blue
+
+# Alternative color schemes (commented out)
+# Lavender
+# export BAR_COLOR=0xff1e1e2e
+# export ITEM_BG_COLOR=0xff313244
+# export ACCENT_COLOR=0xffb4befe
+
+# Mauve
+# export BAR_COLOR=0xff1e1e2e
+# export ITEM_BG_COLOR=0xff313244
+# export ACCENT_COLOR=0xffcba6f7
+
+# Red
+# export BAR_COLOR=0xff1e1e2e
+# export ITEM_BG_COLOR=0xff313244
+# export ACCENT_COLOR=0xfff38ba8
+
+# Green
+# export BAR_COLOR=0xff1e1e2e
+# export ITEM_BG_COLOR=0xff313244
+# export ACCENT_COLOR=0xffa6e3a1
+
+# Peach
+# export BAR_COLOR=0xff1e1e2e
+# export ITEM_BG_COLOR=0xff313244
+# export ACCENT_COLOR=0xfffab387
+
+# Yellow
+# export BAR_COLOR=0xff1e1e2e
+# export ITEM_BG_COLOR=0xff313244
+# export ACCENT_COLOR=0xfff9e2af
