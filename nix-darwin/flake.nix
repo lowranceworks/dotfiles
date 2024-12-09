29fe9bc6245975a24f79b68e7a36ebc62e4cfe39
lowranceworks <lowranceworks@gmail.com>
2024-12-09 11:50:09 -0600

    fix: nix: revert to old config with changes

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -5,190 +5,162 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    dotfiles = {
+      # TODO: find a way to do this without hardcoding the user
+      url = "path:/Users/Joshua.lowrance/projects/lowranceworks/dotfiles";
+      flake = false;
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, dotfiles }:
   let
     configuration = { pkgs, ... }: {
-        environment.systemPath = [
-          "/opt/homebrew/bin"
-          "/usr/local/bin"
-          "/usr/bin"
-          "/bin"
-          "/usr/sbin"
-          "/sbin"
+      ids.gids.nixbld = 350; # NOTE: this is required on my personal MacBook
+      environment.systemPackages = [
+        # pkgs.vim
+        # pkgs.direnv
+        # pkgs.age
+        # pkgs.sshs
+        # pkgs.atac
+        # pkgs.termshark
+        # pkgs.portal
+        # pkgs.glow
+      ];
+      services.nix-daemon.enable = true;
+      nix.settings.experimental-features = "nix-command flakes";
+      programs.zsh.enable = true;
+      programs.fish.enable = true;
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+      system.stateVersion = 4;
+      security.pam.enableSudoTouchIdAuth = true;
+
+      users.users.joshua.home = "/Users/Joshua.lowrance";
+      nix.configureBuildUsers = true;
+      nix.useDaemon = true;
+
+      system.defaults = {
+        dock.orientation = "right";
+        dock.autohide = true;
+        dock.mru-spaces = false;
+        dock.persistent-apps = [
+          "/System/Applications/Launchpad.app/"
+          "/System/Applications/Calendar.app/"
+          "/Applications/Inkdrop.app"
+          "/Applications/WezTerm.app/"
+          "/Applications/Arc.app/"
+          "/Applications/Bitwarden.app/"
+          "/Applications/draw.io.app/"
         ];
-
-        environment.pathsToLink = [ "/Applications" "/bin" ];
-
-        environment.profiles = [
-          "/nix/var/nix/profiles/default"
-          "/run/current-system/sw"
-          "/etc/profiles/per-user/josh"
-        ];
-
-        environment.shells = [
-          "${pkgs.fish}/bin/fish"
-        ];
-
-        environment.loginShell = "${pkgs.fish}/bin/fish";
-
-        users.users.josh = {
-          home = "/Users/josh";
-          shell = "${pkgs.fish}/bin/fish";
-        };
-
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-        system.stateVersion = 4;
-        security.pam.enableSudoTouchIdAuth = true;
-
-        home-manager.backupFileExtension = "backup";
-        nix.configureBuildUsers = true;
-        nix.useDaemon = true;
-
-        system.defaults = {
-          dock.orientation = "bottom";
-          dock.autohide = true;
-          dock.mru-spaces = false;
-          dock.persistent-apps = [
-            "/Applications/WezTerm.app/"
-            "/Applications/SigmaOS.app/"
-            "/Applications/Inkdrop.app"
-            "/System/Applications/Calendar.app/"
-            "/Applications/Bitwarden.app/"
-            "/Applications/draw.io.app/"
-            "/System/Applications/Launchpad.app/"
-          ];
-
-          spaces.spans-displays = true;
-          loginwindow.LoginwindowText = "Let's Go!";
-          screensaver.askForPasswordDelay = 10;
-
-          finder = {
-            ShowPathbar = true;
-            AppleShowAllFiles = true;
-            FXPreferredViewStyle = "clmv";
-            AppleShowAllExtensions = true;
-          };
-
-          NSGlobalDomain = {
-            AppleInterfaceStyle = "Dark";
-            InitialKeyRepeat = 17;
-            KeyRepeat = 2;
-            _HIHideMenuBar = true;
-          };
-        };
-
-        homebrew = {
-          enable = true;
-          onActivation = {
-            autoUpdate = true;
-            upgrade = true;
-          };
-          global = {
-            brewfile = true;
-            lockfiles = true;
-          };
-          taps = [
-            "homebrew/services"
-            "FelixKratz/formulae"
-          ];
-          brews = [
-            "act"
-            "ansible"
-            "argocd"
-            "atuin"
-            "carapace"
-            "checkov"
-            "direnv"
-            "docker"
-            "docker-completion"
-            "docker-compose"
-            "fd"
-            "fish"
-            "fontconfig"
-            "fzf"
-            "gh"
-            "gnumake"
-            "gnused"
-            "go"
-            "go-task"
-            "google-cloud-sdk"
-            "jq"
-            "kind"
-            "kubectl"
-            "kubectx"
-            "kustomize"
-            "krew"
-            "lazydocker"
-            "lazygit"
-            "lsd"
-            "lua"
-            "neofetch"
-            "neovim"
-            "nodejs"
-            "nushell"
-            "podman"
-            "pre-commit"
-            "qemu"
-            "ripgrep"
-            "sketchybar"
-            "sops"
-            "starship"
-            "stern"
-            "stow"
-            "terraform-docs"
-            "terragrunt"
-            "terrascan"
-            "tfenv"
-            "tflint"
-            "tfsec"
-            "tfupdate"
-            "tldr"
-            "tmux"
-            "vim"
-            "yq"
-            "yubikey-agent"
-            "zoxide"
-            "zsh-autosuggestions"
-            "zsh-syntax-highlighting"
-            "zsh-vi-mode"
-            "zstd"
-          ];
-          casks = [
-            "SigmaOS"
-            "alfred"
-            "bitwarden"
-            "cleanshot"
-            "contexts"
-            "drawio"
-            "google-chrome"
-            "inkdrop"
-            "keycastr"
-            "meetingbar"
-            "mission-control-plus"
-            "paintbrush"
-            "slack"
-            "spacelauncher"
-            "stats"
-            "wezterm"
-          ];
-        };
+        finder.AppleShowAllExtensions = true;
+        finder.FXPreferredViewStyle = "clmv";
+        loginwindow.LoginwindowText = "Joshua Lowrance";
+        screencapture.location = "~/Pictures/screenshots";
+        screensaver.askForPasswordDelay = 10;
       };
+
+      homebrew.enable = true;
+      homebrew.casks = [
+        "alfred"
+        "bitwarden"
+        "brave-browser"
+        "chromium"
+        "cleanshot"
+        "contexts"
+        "drawio"
+        "fork"
+        "google-cloud-sdk"
+        "inkdrop"
+        "keycastr"
+        "meetingbar"
+        "mission-control-plus"
+        "paintbrush"
+        # "podman-desktop"
+        # "rectangle-pro"
+        # "sensiblesidebuttons"
+        # "sf-symbols" # NOTE: commented out because it prompts for password
+        "slack"
+        "spacelauncher"
+        "stats"
+        "utm"
+        "wezterm"
+        "betterdisplay"
+        # "homebrew/cask-fonts/font-fontawesome" # WARN: this fails to install because it is deprecated 
+        # "homebrew/cask-fonts/font-hack-nerd-font" # WARN: this fails to install because it is deprecated 
+
+      ];
+      homebrew.brews = [
+        "act"
+        "ansible"
+        "argocd"
+        "awscli"
+        "azure-cli"
+        "bash"
+        "checkov"
+        "cryptography"
+        "direnv"
+        "docker"
+        "docker-completion"
+        "docker-compose"
+        "docutils"
+        "dtc"
+        "fd"
+        "fish"
+        "fisher"
+        "fontconfig"
+        "fzf"
+        "gh"
+        "gnu-sed"
+        "go"
+        "go-task"
+        "helm"
+        "jq"
+        "kind"
+        "krew"
+        "kubectx"
+        "kubernetes-cli"
+        "kustomize"
+        "lazydocker"
+        "lazygit"
+        "lua"
+        "make"
+        "neofetch"
+        "neovim"
+        "node"
+        "podman"
+        "pre-commit"
+        "pyenv"
+        "qemu"
+        "ripgrep"
+        "sops"
+        "starship"
+        "stern"
+        "telnet"
+        "terraform-docs"
+        "terragrunt"
+        "terrascan"
+        "tfenv"
+        "tflint"
+        "tfsec"
+        "tfupdate"
+        "tldr"
+        "tmux"
+        "yq"
+        "yubikey-agent"
+        "zoxide"
+        "zsh-autosuggestions"
+        "zsh-fast-syntax-highlighting"
+        "zsh-vi-mode"
+        "zstd"
+        "federico-terzi/espanso/espanso" # not found, tap it and try again
+        "minamijoyo/hcledit/hcledit" # not found, tap it and try again
+      ];
+    };
   in
   {
     darwinConfigurations."LowranceWorks" = nix-darwin.lib.darwinSystem {
       system = "aarch64-darwin";
       modules = [
         configuration
-        home-manager.darwinModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-        }
       ];
     };
 
