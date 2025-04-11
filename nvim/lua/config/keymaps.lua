8ad94369375ccfb5e5cd17f3a6d23c36976ccc64
lowranceworks <lowranceworks@gmail.com>
2025-04-11 13:50:58 -0500

    fix: nvim: keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,8 +8,9 @@
 --  NOTE: You can find this in init.lua
 
 -- navigation
--- navigation
 vim.keymap.set("n", "<C-u>", "<C-u>zz") -- Center view when scrolling half-page up
 vim.keymap.set("n", "<C-d>", "<C-d>zz") -- Center view when scrolling half-page down
 vim.keymap.set("n", "#", "#zz") -- Center view when searching backward for word under cursor
 vim.keymap.set("n", "*", "*zz") -- Center view when searching forward for word under cursor
+vim.keymap.set("n", "n", "nzz") -- Center view when moving to next search result
+vim.keymap.set("n", "N", "Nzz") -- Center view when moving to previous search result
