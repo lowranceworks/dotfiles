fdae81b35ed1696811820ca2801d43aaa99190bb
Joshua Lowrance <lowranceworks@gmail.com>
2025-06-27 16:38:13 -0500

    docs: add useful keymap comment block

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -14,3 +14,30 @@ vim.keymap.set("n", "#", "#zz") -- Center view when searching backward for word 
 vim.keymap.set("n", "*", "*zz") -- Center view when searching forward for word under cursor
 vim.keymap.set("n", "n", "nzz") -- Center view when moving to next search result
 vim.keymap.set("n", "N", "Nzz") -- Center view when moving to previous search result
+
+--[[
+USEFUL KEYMAPS TO REMEMBER:
+
+LazyGit:
+- <leader>g  = Toggle LazyGit
+- <leader>gb = Git Blame Line
+- <leader>gc = Commits
+- <leader>gg = LazyGit (Root Dir)
+- <leader>gG = LazyGit (Current Dir)
+- <leader>gl = Git Log
+- <leader>gL = Git Log (Current Dir)
+- <leader>gs = Git Status
+
+Neo-tree:
+- D = Delete without confirmation (works with multiple selections)
+- d = Delete with y/n confirmation
+- c = Copy to clipboard
+- x = Cut to clipboard
+- r = Rename
+- a = Add new file/folder
+
+File Navigation:
+- <leader>fe                     = Toggle Neo-tree
+- <leader>ff or <leader><leader> = Find files
+- <leader>sg or <leader>/        = Live grep
+]]
