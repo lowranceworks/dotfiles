b53ce05551c2fc4739764efe233e4688f409d9e7
Joshua Lowrance <lowranceworks@gmail.com>
2025-06-27 16:09:09 -0500

    feat: nvim: `:set ma` is now automatic
    Resolves E21: Cannot make changes, 'modifiable' is off

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Make all buffers modifiable by default (resolves E21: Cannot make changes, 'modifiable' is off)
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("SetModifiable", { clear = true }),
+  pattern = "*",
+  callback = function()
+    vim.bo.modifiable = true
+  end,
+})
