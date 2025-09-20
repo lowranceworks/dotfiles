1ffd6a8213dca507b3fe6450dc4d703f9bf56801
Joshua Lowrance <lowranceworks@gmail.com>
2025-09-20 12:44:16 -0500

    feat: update config

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,6 +2,19 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- Basic autocmds
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Highlight on yank
+autocmd("TextYankPost", {
+  group = augroup("highlight_yank", { clear = true }),
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ timeout = 200 })
+  end,
+})
+
 -- Make all buffers modifiable by default (resolves E21: Cannot make changes, 'modifiable' is off)
 vim.api.nvim_create_autocmd("BufEnter", {
   group = vim.api.nvim_create_augroup("SetModifiable", { clear = true }),
