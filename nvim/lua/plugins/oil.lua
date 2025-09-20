5584f88f0993d1c9692d6d1af325ccdd40289ce0
Joshua Lowrance <lowranceworks@gmail.com>
2025-09-20 12:44:36 -0500

    chore: remove plugins

--- nvim/lua/plugins/oil.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "stevearc/oil.nvim",
-  lazy = true, -- Load when keymaps are triggered (saves startup time)
-  enabled = false,
-  config = function()
-    require("oil").setup({})
-    vim.oil = require("oil")
-  end,
-}
