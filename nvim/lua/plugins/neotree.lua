53312b3eaf383e35e5b784fc48f63034fcf9075f
Joshua Lowrance <lowranceworks@gmail.com>
2025-09-20 12:46:01 -0500

    feat: add neotree

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,4 +1,31 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  lazy = false, -- Load at startup (needed for immediate access)
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    require("neo-tree").setup({
+      -- Your neo-tree configuration here
+      close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+      popup_border_style = "rounded",
+      enable_git_status = true,
+      enable_diagnostics = true,
+      filesystem = {
+        filtered_items = {
+          visible = false, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = true,
+          hide_gitignored = true,
+        },
+        follow_current_file = {
+          enabled = false, -- This will find and focus the file in the active buffer every time
+        },
+        group_empty_dirs = false, -- when true, empty folders will be grouped together
+        hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+      },
+    })
+  end,
 }
