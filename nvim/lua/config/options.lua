cfb074820d9a0f4dacd8d5966ae3dddc728bb1c4
Joshua Lowrance <lowranceworks@gmail.com>
2025-09-20 13:05:20 -0500

    feat: sync clipboard with system/nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Sync clipboard between OS and Neovim
+vim.opt.clipboard = "unnamedplus"
+
 -- Save undo history
 vim.o.undofile = true
 
