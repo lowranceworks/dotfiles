8d0601e2cee1973f9e0b10f299ec90866a9e9f87
Joshua Lowrance <lowranceworks@gmail.com>
2025-09-20 13:11:49 -0500

    feat: add keymaps, update which-key, install mini-icons dependency for which-key

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,43 +1,52 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Basic keymaps
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
--- [[ Basic Keymaps ]]
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: You can find this in init.lua
+-- Telescope keymaps (default/common ones)
+vim.keymap.set("n", "<leader><leader>", ":Telescope find_files<CR>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>/", ":Telescope live_grep<CR>", { desc = "Live grep" })
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Live grep" })
+vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "Find buffers" })
+vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>", { desc = "Help tags" })
+vim.keymap.set("n", "<leader>fr", ":Telescope oldfiles<CR>", { desc = "Recent files" })
+vim.keymap.set("n", "<leader>fc", ":Telescope commands<CR>", { desc = "Commands" })
+vim.keymap.set("n", "<leader>fk", ":Telescope keymaps<CR>", { desc = "Keymaps" })
 
--- navigation
-vim.keymap.set("n", "<C-u>", "<C-u>zz") -- Center view when scrolling half-page up
-vim.keymap.set("n", "<C-d>", "<C-d>zz") -- Center view when scrolling half-page down
-vim.keymap.set("n", "#", "#zz") -- Center view when searching backward for word under cursor
-vim.keymap.set("n", "*", "*zz") -- Center view when searching forward for word under cursor
-vim.keymap.set("n", "n", "nzz") -- Center view when moving to next search result
-vim.keymap.set("n", "N", "Nzz") -- Center view when moving to previous search result
+-- LazyGit keymaps
+vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { desc = "LazyGit" })
+vim.keymap.set("n", "<leader>gb", ":LazyGitFilter<CR>", { desc = "Git Blame" })
+vim.keymap.set("n", "<leader>gc", ":LazyGitFilterCurrentFile<CR>", { desc = "Git Commits" })
+vim.keymap.set("n", "<leader>gl", ":LazyGitFilter<CR>", { desc = "Git Log" })
 
---[[
-USEFUL KEYMAPS TO REMEMBER:
+-- Neo-tree keymaps
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+vim.keymap.set("n", "<leader>fe", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree (File Explorer)" })
 
-LazyGit:
-- <leader>g  = Toggle LazyGit
-- <leader>gb = Git Blame Line
-- <leader>gc = Commits
-- <leader>gg = LazyGit (Root Dir)
-- <leader>gG = LazyGit (Current Dir)
-- <leader>gl = Git Log
-- <leader>gL = Git Log (Current Dir)
-- <leader>gs = Git Status
+-- Other plugin keymaps
+vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>", { desc = "Toggle Undotree" })
+vim.keymap.set("n", "<leader>o", ":Oil<CR>", { desc = "Open Oil" })
 
-Neo-tree:
-- D = Delete without confirmation (works with multiple selections)
-- d = Delete with y/n confirmation
-- c = Copy to clipboard
-- x = Cut to clipboard
-- r = Rename
-- a = Add new file/folder
+-- Harpoon keymaps
+vim.keymap.set("n", "<leader>ha", ":lua require('harpoon.mark').add_file()<CR>", { desc = "Harpoon add file" })
+vim.keymap.set("n", "<leader>hh", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", { desc = "Harpoon menu" })
+vim.keymap.set("n", "<leader>h1", ":lua require('harpoon.ui').nav_file(1)<CR>", { desc = "Harpoon file 1" })
+vim.keymap.set("n", "<leader>h2", ":lua require('harpoon.ui').nav_file(2)<CR>", { desc = "Harpoon file 2" })
+vim.keymap.set("n", "<leader>h3", ":lua require('harpoon.ui').nav_file(3)<CR>", { desc = "Harpoon file 3" })
+vim.keymap.set("n", "<leader>h4", ":lua require('harpoon.ui').nav_file(4)<CR>", { desc = "Harpoon file 4" })
 
-File Navigation:
-- <leader>fe                     = Toggle Neo-tree
-- <leader>ff or <leader><leader> = Find files
-- <leader>sg or <leader>/        = Live grep
-]]
+-- LSP keymaps (add these when you set up LSP)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, { desc = "Workspace symbol" })
+vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, { desc = "Open diagnostic float" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, { desc = "Code action" })
+vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, { desc = "References" })
+vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, { desc = "Rename" })
+vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { desc = "Signature help" })
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,24 +1,22 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
+  dependencies = {
+    "echasnovski/mini.icons",
+  },
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
   end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-  },
   config = function()
     local wk = require("which-key")
     
-    -- Register key groups for better organization
-    wk.register({
-      ["<leader>f"] = { name = "Find" },
-      ["<leader>g"] = { name = "Git" },
-      ["<leader>h"] = { name = "Harpoon" },
-      ["<leader>v"] = { name = "LSP" },
-      ["<leader>w"] = { name = "Workspace" },
+    -- Use the new spec format
+    wk.add({
+      { "<leader>f", group = "Find" },
+      { "<leader>g", group = "Git" },
+      { "<leader>h", group = "Harpoon" },
+      { "<leader>v", group = "LSP/Code" },
     })
   end,
 }
