b5bd4b316000dfaa9b616088d71a9dc69c1546e9
Joshua Lowrance <lowranceworks@gmail.com>
2025-09-22 09:44:16 -0500

    fix: harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,23 +1,39 @@
 return {
   "ThePrimeagen/harpoon",
   lazy = false, -- Load at startup (needed for immediate access)
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  keys = {
-    { "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", 
-      desc = "Harpoon add file" },
-    { "<leader>hh", 
-      "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", 
-      desc = "Harpoon menu" },
-    { "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", 
-      desc = "Harpoon file 1" },
-    { "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", 
-      desc = "Harpoon file 2" },
-    { "<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", 
-      desc = "Harpoon file 3" },
-    { "<leader>h4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", 
-      desc = "Harpoon file 4" },
-  },
+  dependencies = { "nvim-lua/plenary.nvim" },
   config = true,
+  keys = {
+    { 
+      "<leader>hm", 
+      "<cmd>lua require('harpoon.mark').add_file()<cr>", 
+      desc = "Mark file with harpoon" 
+    },
+    { 
+      "<leader>hn", 
+      "<cmd>lua require('harpoon.ui').nav_next()<cr>", 
+      desc = "Go to next harpoon mark" 
+    },
+    { 
+      "<leader>hp", 
+      "<cmd>lua require('harpoon.ui').nav_prev()<cr>", 
+      desc = "Go to previous harpoon mark" 
+    },
+    { 
+      "<leader>ha", 
+      "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", 
+      desc = "Show harpoon marks" 
+    },
+  },
+  config = function()
+    require("harpoon").setup({
+      global_settings = {
+        save_on_toggle = false,
+        save_on_change = true,
+        enter_on_sendcmd = false,
+        tmux_autoclose_windows = false,
+        excluded_filetypes = { "harpoon" },
+      },
+    })
+  end,
 }
