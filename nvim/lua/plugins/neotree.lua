c077ee6612ac7bcb637a6ef5376f79aade24eb84
Joshua Lowrance <lowranceworks@gmail.com>
2025-09-22 09:48:56 -0500

    fix: neotree width

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -15,7 +15,47 @@ return {
   },
   config = function()
     require("neo-tree").setup({
-      -- Your existing config...
+      close_if_last_window = false,
+      popup_border_style = "rounded",
+      enable_git_status = true,
+      enable_diagnostics = true,
+      default_component_configs = {
+        container = {
+          enable_character_fade = true
+        },
+        indent = {
+          indent_size = 2,
+          padding = 1,
+          with_markers = true,
+          indent_marker = "│",
+          last_indent_marker = "└",
+          highlight = "NeoTreeIndentMarker",
+          with_expanders = nil,
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+      },
+      window = {
+        position = "left",
+        width = 55, -- Make neo-tree wider (default is 25)
+        mapping_options = {
+          noremap = true,
+          nowait = true,
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          visible = false,
+          hide_dotfiles = true,
+          hide_gitignored = true,
+        },
+        follow_current_file = {
+          enabled = false,
+        },
+        group_empty_dirs = false,
+        hijack_netrw_behavior = "open_default",
+      },
     })
     
     -- Auto-open neo-tree when starting nvim with a directory
