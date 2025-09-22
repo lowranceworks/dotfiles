6f497a7a6a8bacf1a681fb1a2c256ccf08de2600
Joshua Lowrance <lowranceworks@gmail.com>
2025-09-22 09:07:40 -0500

    feat: add conform

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,32 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    formatters_by_ft = {
+      javascript = {},
+      typescript = {},
+      javascriptreact = {},
+      typescriptreact = {},
+      svelte = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+      graphql = { "prettier" },
+      liquid = { "prettier" },
+      lua = { "stylua" },
+      python = { "isort", "black" },
+    },
+    format_on_save = {
+      lsp_fallback = true,
+      async = false,
+      timeout_ms = 1000,
+    },
+    formatters = {
+      prettier = {
+        prepend_args = { "--tab-width", "2" },
+      },
+    },
+  },
+}
