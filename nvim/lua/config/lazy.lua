757b40685cfbec565607c2c474ea7187889f3bb2
Joshua Lowrance <lowranceworks@gmail.com>
2025-09-24 08:34:23 -0500

    feat: add config/lazy.lua

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,37 +1,32 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
   spec = {
-    -- Only import the LazyVim base configuration
+    -- add LazyVim and import its plugins
     {
       "LazyVim/LazyVim",
-      import = "lazyvim.config",
+      import = "lazyvim.plugins",
       opts = {
-        defaults = {
-          autocmds = true, -- lazyvim.config.autocmds
-          keymaps = true, -- lazyvim.config.keymaps
-          options = true, -- lazyvim.config.options
+        news = {
+          lazyvim = true,
+          neovim = true,
         },
       },
     },
-    -- Import only your custom plugins
+    -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
     lazy = false,
     version = false,
   },
+  checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
       disabled_plugins = {
