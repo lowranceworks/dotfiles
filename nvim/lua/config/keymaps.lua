032eaf1cf77adbedcf3774c8b26f128012d12e0e
Joshua Lowrance <lowranceworks@gmail.com>
2025-09-24 09:41:03 -0500

    fix: keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,11 +1,14 @@
 -- Plugin keymaps are defined in their respective plugin files for lazy loading
--- Telescope: <leader><leader>, <leader>/, <leader>f*
--- LazyGit: <leader>g*
--- Neo-tree: <leader>e, <leader>fe
+-- Snacks Explorer: <leader>e, <leader>fe
+-- Snacks Picker (find/search): <leader><leader>, <leader>/, <leader>f*
+-- Snacks Git: <leader>g* (lazygit, blame, commits, log)
+-- Snacks Buffers: <leader>bd, <S-h>, <S-l>, [b, ]b, <leader>bo
+-- Snacks Undo: <leader>u
+-- Snacks Notifications: <leader>n, <leader>un
 -- Harpoon: <leader>h*
--- Oil: <leader>o
--- Undotree: <leader>u
 -- LSP: gd, K, <leader>v*, [d, ]d
+-- Comment: gcc, gc (native or plugin)
+-- Which-key: <leader> (shows available keys)
 
 -- Basic vim keymaps (non-plugin)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -14,3 +17,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- Keep visual selection when indenting
+vim.keymap.set('v', '<', '<gv', { desc = 'Indent left and reselect' })
+vim.keymap.set('v', '>', '>gv', { desc = 'Indent right and reselect' })
