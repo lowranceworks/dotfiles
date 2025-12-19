3576c96e5191f4641d48548b6a3009e07bea5d40
Joshua Lowrance <lowranceworks@gmail.com>
2025-12-19 13:19:32 -0600

    refactor: sketchybar

--- sketchybar/plugins/aerospacer.sh
+++ sketchybar/plugins/aerospacer.sh
@@ -4,8 +4,7 @@
 FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+  sketchybar --set $NAME background.drawing=on
 else
-    sketchybar --set $NAME background.drawing=off
+  sketchybar --set $NAME background.drawing=off
 fi
-
