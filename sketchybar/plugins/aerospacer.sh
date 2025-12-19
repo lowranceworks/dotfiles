73440f93e83cb8ae136b95789ae52c5b60e58640
Joshua Lowrance <lowranceworks@gmail.com>
2025-12-19 12:58:54 -0600

    fix: sketchybar: add sketchybar

---
+++ sketchybar/plugins/aerospacer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the currently focused workspace from aerospace
+FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
+
+if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+    sketchybar --set $NAME background.drawing=on
+else
+    sketchybar --set $NAME background.drawing=off
+fi
+
