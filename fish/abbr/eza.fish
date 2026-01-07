723e6584b3b6bbcc6719bc3d66b59f40d110212d
Joshua Lowrance <lowranceworks@gmail.com>
2026-01-07 14:16:53 -0600

    fix: fish: ignore .terraform directory

--- fish/abbr/eza.fish
+++ fish/abbr/eza.fish
@@ -3,5 +3,5 @@
 abbr l "eza --group-directories-first -Al"
 abbr ll "eza --all --git --icons --color=always --group-directories-first -Al"
 abbr lt "eza --group-directories-last --tree"
-abbr lta "eza --group-directories-last --tree --all --ignore-glob='.git|node_modules|.venv'"
+abbr lta "eza --group-directories-last --tree --all --ignore-glob='.git|node_modules|.venv|.terraform|'"
 abbr ltd "eza --group-directories-last --tree --only-dirs"
