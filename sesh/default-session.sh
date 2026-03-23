2d0116fb199b7bce7b133bc7740a235758492fea
Joshua Lowrance <lowranceworks@gmail.com>
2026-03-23 10:20:41 -0500

    fix: sesh: default session

--- sesh/default-session.sh
+++ sesh/default-session.sh
@@ -5,6 +5,7 @@ tmux rename-window -t 1 nvim
 # Create additional windows with names
 tmux new-window -n lazygit
 tmux new-window -n fish
+tmux new-window -n fish
 
 # Send commands to each window
 tmux send-keys -t 1 'nvim .' Enter
@@ -12,4 +13,4 @@ tmux send-keys -t lazygit 'lazygit' Enter
 tmux send-keys -t fish 'eza --group-directories-last --tree' Enter
 
 # Focus on editor window
-tmux select-window -t fish
+tmux select-window -t nvim
