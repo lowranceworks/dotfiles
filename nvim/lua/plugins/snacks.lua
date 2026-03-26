edab9b266f65e930041c93b9d1a9de28a8d598dc
Joshua Lowrance <lowranceworks@gmail.com>
2026-03-26 11:34:48 -0500

    feat: nvim: add vim bindings to snacks.lua
    Allows you to navigate files with ctrl+d and ctrl+u.

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -226,6 +226,16 @@ return {
               position = "left",
             },
           },
+          win = {
+            list = {
+              keys = {
+                ["<C-h>"] = { "<cmd>TmuxNavigateLeft<cr>", mode = { "n", "i" } },
+                ["<C-j>"] = { "<cmd>TmuxNavigateDown<cr>", mode = { "n", "i" } },
+                ["<C-k>"] = { "<cmd>TmuxNavigateUp<cr>", mode = { "n", "i" } },
+                ["<C-l>"] = { "<cmd>TmuxNavigateRight<cr>", mode = { "n", "i" } },
+              },
+            },
+          },
         },
       },
     },
