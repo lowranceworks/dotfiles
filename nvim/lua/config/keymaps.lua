850db850149afc4715f82f8a9e14e23080f33b82
Joshua Lowrance <Joshua.Lowrance@mlb.com>
2026-05-01 10:50:38 -0500

    fix: nvim: remove keymaps that center with zz

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,10 +5,6 @@
 -- Basic vim keymaps (non-plugin)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
 
 -- Keep visual selection when indenting
 vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
